b0604904c7f475e0bfc69bd92460385641925be7
ssk801 <77089746+ssk801@users.noreply.github.com>
2021-02-01 14:31:49 +1100

    Update 2d6-histo.py

--- 2d6-histo.py
+++ 2d6-histo.py
@@ -1,3 +1,8 @@
+#01feb2021
+#generates a list of figures representing the frequency of sums from the rolling of 2d6
+#min sum is 2, max is 12 so there are 11 possible outcomes
+#note -2 index offset to cater for this and not have empty places in the list
+
 import random
 
 def d6():
